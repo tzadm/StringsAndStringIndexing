f0286bf0c84d245d269092ab1757b220b6713d49
tzadm <94993624+tzadm@users.noreply.github.com>
2024-05-09 16:54:07 +0300

    Add files via upload

--- StringsAndStringIndexing.py
+++ StringsAndStringIndexing.py
@@ -1,7 +1,7 @@
-MyLine = 'Это моя строка'
-print('result #1: ', MyLine[0])
-print('result #2: ', MyLine[-1])
-print('result #3: ', MyLine[3:6])
-print('result #4: ', MyLine[::-1])
-print('result #5: ', len(MyLine))
-print('result #6: ', 'Это новая строка ' + MyLine)
+my_Line = 'Это моя строка'
+print('result #1: ', my_Line[0])
+print('result #2: ', my_Line[-1])
+print('result #3: ', my_Line[2:5])
+print('result #4: ', my_Line[::-1])
+print('result #5: ', len(my_Line))
+print('result #6: ', 'Это новая строка ' + my_Line)
