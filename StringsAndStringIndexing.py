f5d1d34751b6938778a80fca24b2dc1ee261fc62
tzadm <94993624+tzadm@users.noreply.github.com>
2024-05-09 15:44:35 +0300

    Add files via upload

---
+++ StringsAndStringIndexing.py
@@ -0,0 +1,7 @@
+MyLine = 'Это моя строка'
+print('result #1: ', MyLine[0])
+print('result #2: ', MyLine[-1])
+print('result #3: ', MyLine[3:6])
+print('result #4: ', MyLine[::-1])
+print('result #5: ', len(MyLine))
+print('result #6: ', 'Это новая строка ' + MyLine)
